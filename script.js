b99618be2d23fd234654d0ec1db7ddfc84ec28d2
enwkurs <erinwajca@gmail.com>
2025-10-11 22:40:16 +0200

    it pretty now

--- script.js
+++ script.js
@@ -8,7 +8,8 @@ function XaddListItem(event){
   console.log("[BUTTON CLICKED]");
 
 // Get the value inside the input box
-  const XtodoInputVALUE = document.querySelector("#XtodoInputBox").value;
+  const XtodoInputBOX = document.querySelector("#XtodoInputBox");
+  const XtodoInputVALUE = XtodoInputBOX.value;
   console.log(`[DATA FOUND] ${XtodoInputVALUE}`);
 
 // Create a new <li> and give it a class name
@@ -20,12 +21,16 @@ function XaddListItem(event){
 // Define the <div> text content as the content of the input box
   XtodoTextDIV.textContent = XtodoInputVALUE;
   XtodoTextDIV.className = "XtodoTextDIV";
-// XtodoTextDIV.addEventListener("change", XtodoStrike); REMOVE FOR NOW
 
 // Create a new input element and make it a checkbox
   const XtodoCheckbox = document.createElement("input");
   XtodoCheckbox.type = "checkbox";
   XtodoCheckbox.id = "XtodoCheckboxElement";
+
+// Create a new <span> to become the custom checkbox
+  const customBox = document.createElement("span");
+  customBox.className = "customBox";
+
 // Add event listener to detect change in checkbox state
 // Define a function to happen when the state of the checkbox changes
   XtodoCheckbox.addEventListener("change", function (event) {
@@ -33,9 +38,31 @@ function XaddListItem(event){
 
     if (event.target.checked) {
       XtodoTextDIV.classList.add("strike")
+      XtodoDisplayLI.classList.add("fade")
+      customBox.classList.add("customBoxChecked")
+      customBox.textContent = "✓"
+
     } else {
       XtodoTextDIV.classList.remove("strike")
+      XtodoDisplayLI.classList.remove("fade")
+      customBox.classList.remove("customBoxChecked")
+      customBox.textContent = ""
     }
+  });
+
+// Create a new input element and make it a button
+  const XremoveButton = document.createElement("button");
+  XremoveButton.textContent = "X";
+  XremoveButton.className = "formButton";
+  XremoveButton.id = "XremoveButton";
+  // Add event listening to detect when the button is clicked
+  // Define a function to happen when the button is clicked
+  XremoveButton.addEventListener("click", function (event) {
+  event.preventDefault();
+  console.log("[REMOVE BUTTON] Button Clicked.");
+
+  XtodoDisplayLI.remove();
+
   });
 
 // Create a reference point to the <ul>
@@ -46,7 +73,12 @@ function XaddListItem(event){
 
 // Append Elements
   XtodoDisplayLI.appendChild(XtodoCheckbox);
+  XtodoDisplayLI.appendChild(customBox);
   XtodoDisplayLI.appendChild(XtodoTextDIV);
+  XtodoDisplayLI.appendChild(XremoveButton);
+
+// Reset the input box
+XtodoInputBOX.value = "";
 }
 
 // Set up trigger that makes the button work -------------
